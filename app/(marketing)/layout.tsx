6ce38bc1e1ddebb679320c798cd262253828fb10
TanLeJS <tanlengoc2000@gmail.com>
2024-03-17 17:50:35 -0400

    authentication

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,3 +1,4 @@
+
 import { Footer } from "./footer";
 import { Header } from "./header";
 
@@ -6,14 +7,14 @@ type Props = {
 };
 const MarketingLayout = ({ children }: Props) => {
   return (
-    <div className="min-h-screen flex flex-col">
-      <Header />
-      <main className="flex-1 flex flex-col items-center justify-center">
-        {" "}
-        {children}
-      </main>
-      <Footer />
-    </div>
+      <div className="min-h-screen flex flex-col">
+        <Header />
+        <main className="flex-1 flex flex-col items-center justify-center">
+          {" "}
+          {children}
+        </main>
+        <Footer />
+      </div>
   );
 };
 export default MarketingLayout;
